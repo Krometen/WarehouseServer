f358dfb7888eda9d6e28ac549a338e73d8cfd3e0
vladimir-v-s <vladimirvsu01@mail.ru>
2021-04-27 19:55:07 +0300

    Пометил репозитории анитацией @Repository.

--- complete/src/main/java/com/warehouse/server/repos/OrderRepo.java
+++ complete/src/main/java/com/warehouse/server/repos/OrderRepo.java
@@ -2,11 +2,13 @@ package com.warehouse.server.repos;
 
 import com.warehouse.server.model.OrderEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 
 //Hibernate репозиторий заказов
+@Repository
 public interface OrderRepo extends CrudRepository<OrderEntity, Long> {
     List<OrderEntity> findAll();
 }
--- complete/src/main/java/com/warehouse/server/repos/ProductRepo.java
+++ complete/src/main/java/com/warehouse/server/repos/ProductRepo.java
@@ -2,11 +2,13 @@ package com.warehouse.server.repos;
 
 import com.warehouse.server.model.ProductEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 
 //Hibernate репозиторий товаров
+@Repository
 public interface ProductRepo extends CrudRepository<ProductEntity, Long> {
     List<ProductEntity> findAll();
 }
